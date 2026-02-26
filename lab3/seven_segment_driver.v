09920bcc3d5e7b953e2c87ecb53a8b5043b42eff
Ben Petlach <bpetlach@gsp12.eecs.yorku.ca>
2026-02-26 15:15:20 -0500

    updated comments. driver was fixed

--- lab3/seven_segment_driver.v
+++ lab3/seven_segment_driver.v
@@ -3,12 +3,6 @@ module seven_segment_driver (
 	output [7:0] seg_out
 );
 
-	//NOTE: C is wrong (top not on)
-	//NOTE: D is wrong (top on)
-	//NOTE: E is wrong (top not on)
-	//NOTE: F is wrong (top not on)
-	//NOTE: 4 is wrong (top on)
-
 	assign seg_out[0] = (num_in[0] & num_in[1] & ~num_in[2] & num_in[3]) |			  
 							  (num_in[0] & ~num_in[1] & num_in[2] & num_in[3]) |
 							  (~num_in[0] & ~num_in[1] & num_in[2] & ~num_in[3]) |
