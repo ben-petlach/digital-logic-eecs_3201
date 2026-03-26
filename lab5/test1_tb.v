aaf9f68c899adf5912e4823ef75f1d8256517b59
Ben Petlach <ben.petlach@gmail.com>
2026-03-26 14:08:04 -0400

    lab5 testbenches

---
+++ lab5/test1_tb.v
@@ -0,0 +1,64 @@
+`timescale 1ns/1ps
+
+module test1_tb();
+    reg tb_a, tb_b, tb_c;
+    wire tb_x, tb_y;
+
+    test1 DUT(
+        .a(tb_a),
+        .b(tb_b),
+        .c(tb_c),
+        .x(tb_x),
+        .y(tb_y)
+    );
+
+    always @(tb_x, tb_y) begin
+        if (tb_x == 0 && tb_y == 0) begin
+            $display("All outputs are zero");
+        end
+    end
+
+    initial begin
+        tb_a = 0;
+        tb_b = 0;
+        tb_c = 0;
+
+        #20;
+        tb_a = 0;
+        tb_b = 0;
+        tb_c = 1;
+
+        #20;
+        tb_a = 0;
+        tb_b = 1;
+        tb_c = 0;
+
+        #20;
+        tb_a = 0;
+        tb_b = 1;
+        tb_c = 1;
+
+        #20;
+        tb_a = 1;
+        tb_b = 0;
+        tb_c = 0;
+
+        #20;
+        tb_a = 1;
+        tb_b = 0;
+        tb_c = 1;
+
+        #20;
+        tb_a = 1;
+        tb_b = 1;
+        tb_c = 0;
+
+        #20;
+        tb_a = 1;
+        tb_b = 1;
+        tb_c = 1;
+
+        $stop;
+    end
+
+endmodule
---
+++ lab5/test2_tb.v
@@ -0,0 +1,38 @@
+`timescale 1ns / 1ps
+
+module test2_tb();
+    reg tb_clk;
+    reg tb_a;
+    wire [3:0] tb_r;
+
+    test2 DUT (
+        .clk(tb_clk),
+        .a(tb_a),
+        .r(tb_r)
+    );
+
+    always #10 tb_clk = ~tb_clk;
+
+    always @(tb_r) begin
+        if (tb_r == 4'b0000) begin
+            $display("All outputs are zero");
+        end
+    end
+
+    initial begin
+        tb_clk = 0;
+        tb_a = 0;
+
+        #100;
+
+        tb_a = 1;
+
+        #200;
+
+        tb_a = 0;
+
+        #100;
+
+        $stop;
+    end
+endmodule
